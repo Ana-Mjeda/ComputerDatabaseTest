41a5123d898483f813b5a215383709ab23dac590
ana.mjeda <anamjeda@gmail.com>
2023-02-07 15:34:51 +0100

    added new test

--- src/test/java/tests/AddNewComputer.java
+++ src/test/java/tests/AddNewComputer.java
@@ -6,13 +6,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
+import pages.AddNewPage;
 import pages.HomePage;
 
 import java.time.Duration;
 
-public class SearchTest {
+public class AddNewComputer{
 
     protected WebDriver driver;
 
@@ -20,11 +19,14 @@ public class SearchTest {
 
     protected HomePage homePage;
 
+    protected AddNewPage addNewPage;
+
     @BeforeClass
     public void beforeClass() {
         driver = new ChromeDriver();
         driverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
         homePage = new HomePage(driver, driverWait);
+        addNewPage = new AddNewPage(driver, driverWait);
     }
 
     @BeforeMethod
@@ -32,20 +34,11 @@ public class SearchTest {
         driver.get("http://computer-database.gatling.io/computers");
     }
 
-    @Test
-    public void searchTestSoftAssert() {
-        String searchString = "ASCI";
-        homePage.search(searchString);
-        int numberOfComputers = homePage.getComputerList().size();
-        String expMsg = "Displaying 1 to " + numberOfComputers + " of " +numberOfComputers;
 
-        SoftAssert softAssert = new SoftAssert();
-        softAssert.assertTrue(homePage.containsSearchString(searchString));
-        softAssert.assertTrue(driver.getCurrentUrl().endsWith("/computers?f=" + searchString));
-        softAssert.assertTrue(homePage.getTopMessage().startsWith(numberOfComputers + ""));
-        softAssert.assertEquals(homePage.getTableMessage(), expMsg);
-        softAssert.assertAll();
-    }
+
+
+
+
 
     @AfterClass
     public void afterClass() {
