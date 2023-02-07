6cf65598b03215ab4d2bb1a10e42b0e205702791
ana.mjeda <anamjeda@gmail.com>
2023-02-07 15:14:07 +0100

    added findby methods in AddNewPage

--- src/test/java/pages/AddNewPage.java
+++ src/test/java/pages/AddNewPage.java
@@ -1,9 +1,26 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AddNewPage extends BasePage{
+
+    @FindBy (id = "name")
+    private WebElement computerName;
+
+    @FindBy (id = "introduced")
+    private WebElement introducedField;
+
+    @FindBy (id = "discontinued")
+    private WebElement discontinuedField;
+
+    @FindBy (id = "company")
+    private WebElement company;
+
+    @FindBy (xpath = "//*[@id=\"main\"]/form/div/input")
+    private WebElement createComputerButton;
 
     public AddNewPage(WebDriver driver, WebDriverWait driverWait) {
         super(driver, driverWait);
